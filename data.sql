9010fc1d6c5f220e6f193c2f48603eedc0558987
nroach24 <noah_roach@outlook.com>
2025-11-17 17:16:41 -0500

    data.sql change to 1 table

--- data.sql
+++ data.sql
@@ -1,29 +1,14 @@
-CREATE TABLE sports(
+CREATE TABLE cards(
 	id SERIAL,
 	category VARCHAR(100),
-	playerName VARCHAR(100),
-	sport VARCHAR(25),
-	year INT,
+	cardName VARCHAR(100),
 	brand VARCHAR(25),
+	type VARCHAR(50),
+	year INT,
 	cardNumber INT,
 	condition VARCHAR(25),
 	price DECIMAL,
-    description VARCHAR(200),
-    imageURL VARCHAR(500),
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE tcgs(
-	id SERIAL,
-	category VARCHAR(100),
-    cardName VARCHAR(100),
-	tcg VARCHAR(25),
-	set VARCHAR(50),
-	year INT,
-	cardNumber INT,
-	condition VARCHAR(10),
-	price DECIMAL,
-    description VARCHAR(200),
-    imageURL VARCHAR(500),
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  description VARCHAR(200),
+  imageURL VARCHAR(500),
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
