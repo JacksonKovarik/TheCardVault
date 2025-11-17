137e05e38ca412cc5da8d69b506bc042e8418d3c
Jackson Kovarik <jkovarik@umich.edu>
2025-11-17 17:29:20 -0500

    Update endpoints

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const pool = new Pool({
 
 app.get("/api/cards", (req, res) => {
     const sql =
-        "SELECT * FROM sports ORDER BY createdat DESC; SELECT * FROM tcgs ORDER BY createdat DESC;";
+        "SELECT * FROM cards ORDER BY createdat DESC;";
 
     pool.query(sql, (err, result) => {
         if (err) {
@@ -21,16 +21,16 @@ app.get("/api/cards", (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
         } else {
         console.log("Query successful, sending response");
-        res.status(200).json(result[0].rows.concat(result[1].rows));
+        res.status(200).json(result.rows);
         }
     });
 });
 
-app.post("/api/cards/sports", (req, res) => {
+app.post("/api/cards", (req, res) => {
     const data = [
         req.body.category,
-        req.body.playerName,
-        req.body.sport,
+        req.body.cardName,
+        req.body.type,
         req.body.year,
         req.body.brand,
         req.body.cardNumber,
@@ -39,7 +39,7 @@ app.post("/api/cards/sports", (req, res) => {
         req.body.description,
     ];
     const sql =
-        "INSERT INTO sports (category, playerName, sport, year, brand, cardNumber, condition, price, description, createdat) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, Now())";
+        "INSERT INTO cards (category, cardName, type, year, brand, cardNumber, condition, price, description, createdat) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, Now())";
 
     pool.query(sql, data, (err, result) => {
         if (err) {
@@ -51,30 +51,30 @@ app.post("/api/cards/sports", (req, res) => {
         }
     });
 });
-app.post("/api/cards/tcgs", (req, res) => {
-    const data = [
-        req.body.category,
-        req.body.cardName,
-        req.body.tcg,
-        req.body.set,
-        req.body.year,
-        req.body.cardNumber,
-        req.body.condition,
-        req.body.price,
-        req.body.description,
-    ];
-    const sql =
-        "INSERT INTO tcgs (category, cardname, tcg, set, year, cardNumber, condition, price, description, createdat) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, Now())";
-    pool.query(sql, data, (err, result) => {
-        if (err) {
-        console.error("Error executing query", err.stack);
-        res.status(500).json({ error: "Internal Server Error" });
-        } else {
-        console.log("Query successful, sending response");
-        res.status(200).json(result.rows);
-        }
-    });
-});
+// app.post("/api/cards/tcgs", (req, res) => {
+//     const data = [
+//         req.body.category,
+//         req.body.cardName,
+//         req.body.tcg,
+//         req.body.set,
+//         req.body.year,
+//         req.body.cardNumber,
+//         req.body.condition,
+//         req.body.price,
+//         req.body.description,
+//     ];
+//     const sql =
+//         "INSERT INTO tcgs (category, cardname, tcg, set, year, cardNumber, condition, price, description, createdat) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, Now())";
+//     pool.query(sql, data, (err, result) => {
+//         if (err) {
+//         console.error("Error executing query", err.stack);
+//         res.status(500).json({ error: "Internal Server Error" });
+//         } else {
+//         console.log("Query successful, sending response");
+//         res.status(200).json(result.rows);
+//         }
+//     });
+// });
 
 app.listen(80, () => {
     console.log("Server is running on port 80");
